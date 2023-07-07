e588681b3e11840a2728b645c44f000c2f9d76ee
Bhagyashree <bahgyashreemohanty@gmail.com>
2023-07-07 12:48:21 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,5 @@
+document.getElementById("btn1").addEventListener('click',show);
+document.getElementById("btn2").addEventListener('click',show);
+function show(){
+	document.getElementById("popup").style.display ='block';
+}
