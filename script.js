6cf19e77d8a9aa5566aa4e4309f637cec83a2aa2
LBhagyashree <87573548+LBhagyashree@users.noreply.github.com>
2023-07-08 18:42:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById("btn1").addEventListener('click',show);
 document.getElementById("btn2").addEventListener('click',show);
+document.getElementById("clsbtn").addEventListener('click',hide);
 function show(){
 	document.getElementById("popup").style.display ='block';
+}
+function hide(){
+	document.getElementById("popup").style.display ='none';
 }
